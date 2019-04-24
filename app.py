298e5cf2ca19298a2249db07141cf217cd9039dc
Jennifer Browning <browningjen@yahoo.com>
2019-04-24 18:38:14 -0400

    app.py seems to be working ok

--- app.py
+++ app.py
@@ -2,48 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-# from flask import Flask, render_template, jsonify, request
-# app = Flask(__name__)
-
-# todos = {}
-# # app.debug = True
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# @app.route("/todo/")
-# def get_todos():
-#     return jsonify(todo=todos)
-
-# @app.route("/todo/", methods=['POST'])
-# def create_todo():
-#     todo = request.get_json()
-#     if len(todos) == 0:
-#         todos[0] = todo['name']
-#     else:
-#         todos[max(todos.keys()) + 1] = todo['name']
-#     return jsonify(todo=todos)
-
-# @app.route("/todo/<int:id>")
-# def read_todo(id):
-#     todo = todos[id]
-#     return jsonify(todo=todos)
-
-# @app.route("/todo/<int:id>", methods=['PUT'])
-# def update_todo(id):
-#     todo = request.get_json()
-#     todos[id] = todo['name']
-#     return jsonify(todo=todos)
-
-# @app.route("/todo/<int:id>", methods=['DELETE'])
-# def delete_todo(id):
-#     todos.pop(id, None)
-#     return jsonify(todo=todos)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 import datetime
@@ -57,60 +15,69 @@ current_datetime = datetime.datetime.now()
 
 
 def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+    if todo_id not in todo_list:
+        abort(404, message="This To Do Item {} doesn't exist".format(todo_id))
 
 parser = reqparse.RequestParser()
 parser.add_argument('title', type=str, default="Please enter a title")
 parser.add_argument('due_date', type=str, default="When do you need this done?")
 parser.add_argument('completed', type=bool, default=False)
 
-
-# Todo
-# shows a single todo item and lets you delete a todo item
-class Todo(Resource):
+class TodoList(Resource):
+    """Create a To Do List for yo'self"""
     def get(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    def get(self):
-        return TODOS
+        return todo_list
 
     def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        global todo_id
+        args.parser.parse_args()
+        todo_id = int(todo_id) + 1
+        todo_id = str(todo_id)
+        todo_list[todo_id]= {
+            "completed_date": str(current_datetime),
+            "creation_date": str(current_datetime),
+            "last_update": str(current_datetime),
+            "title": args["title"],
+            "completed": args["completed"],
+            "due_date": args["due_date"]
+        }
 
-class Update_todo(Resource):
-    def get(self):
-        return TODOS
+        return todo_id, 201
 
-    def post(self):
-        args = parser.parse_args()
+class TodoItem(Resource):
+    """Create some To Do Items for yo'self"""
+    def get(self, todo_id):
+        return todo_list[todo_id]
+
+    def put(self):
+        """update a todo item"""
+        args.parser.parse_args()
+        if args['title']:
+            todo_list[todo_id].update({'title': args['title']})
+        if args['creation_date']:
+            todo_list[todo_id].update({'created on': str(current_datetime)})
+        if args['last_update']:
+            todo_list[todo_id].update({'Updated on': str(current_datetime)})
+        if args['completed']:
+            todo_list[todo_id].update({'completed': args['completed']})
+            if args['completed']is True:
+                todo_list[todo_id].update({'Completed on': str(current_datetime)})
+            if args['completed']is False:
+                todo_list[todo_id].update({'competed_date': 'Seriously??'})
+        if args['completed_date']:
+            todo_list[todo_id].update({'completed_date': str(current_datetime)})
         
+        return 201
 
-##
-## Actually setup the Api resource routing here
-##
+    def delete_item(self, todo_id):
+        """Deletes a to do item on the list"""
+        del todo_list[todo_id]
+
+        return 204
+     
 api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(TodoItem, '/todos/<todo_id>')
 
 
 if __name__ == '__main__':
